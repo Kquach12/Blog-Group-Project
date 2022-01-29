4260a56c0cd8ac557ff8c19a1a999554dfd517e8
Kenny Quach <kennyquach60@gmail.com>
2022-01-29 14:03:58 -0800

    Add toggle button between My Blogs and All Blogs

--- blog/client/src/views/Home.js
+++ blog/client/src/views/Home.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import BlogList from '../components/BlogList';
 import ProfileInfo from '../components/ProfileInfo';
+import CommentForm from '../components/CommentForm';
 
 
 
@@ -10,25 +11,50 @@ const Home = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState();
   const [loaded, setLoaded] = useState(false);
+  const [showBlogsTest, setShowBlogsTest] = useState(false);
+
+
     useEffect(()=>{
         axios.get('http://localhost:8000/api/blogs')
             .then(res=>{
                 setBlogs(res.data);
                 setLoaded(true);
-            }).then(axios.get('http://localhost:8000/api/user/getLoggedInUser')
-                .then(res => setUser(res.data)));;
+            })
     },[])
+
+    useEffect(()=>{
+      axios.get('http://localhost:8000/api/user/getLoggedInUser', {withCredentials:true})
+          .then(res=>{ setUser(res.data);
+          })
+    },[])
+
+    const changeBlogs = () =>{
+      setShowBlogsTest(!showBlogsTest)
+      console.log(showBlogsTest)
+    }
+
   return (
 
     <div className='container'>
       <div className='row'>
+
         <div className='col'>
+
           <ProfileInfo /> 
+          {/* Use this button to toggle between All Blogs and My Blogs */}
+          {
+            showBlogsTest ?
+              <button onClick={changeBlogs} className='btn btn-primary'>My Blogs</button>
+            :
+              <button onClick={changeBlogs} className='btn btn-primary'>All Blogs</button>
+          }
         </div>
+
         <div className='col'>
           <h2>Blogs start here</h2>
-          { loaded && <BlogList filter={user._id} showUserBlogs={false}/> }
+          <BlogList  showUserBlogs={showBlogsTest}/> 
         </div>
+
       </div>
     </div>
   )
