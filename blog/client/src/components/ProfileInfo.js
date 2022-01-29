e7abcf95c9aa11ffd0b5dcd27be73c9c0a0c7263
Kenny Quach <kennyquach60@gmail.com>
2022-01-28 23:04:19 -0800

    Create ProfileInfo component and update Home view

---
+++ blog/client/src/components/ProfileInfo.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+
+
+const ProfileInfo = (props) => {
+
+    // How to get current user?
+
+
+
+    return (
+        <div>
+            <img src="Img placeholder" />
+            <h2>John Doe</h2>
+        </div>
+    )
+}
+export default ProfileInfo;
