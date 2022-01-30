9e112d480fc5790056f5b961f7dc6050041c2681
nealichinohe <neal.kuni@yahoo.com>
2022-01-29 19:53:37 -0700

    Profile component

--- blog/client/src/components/ProfileInfo.js
+++ blog/client/src/components/ProfileInfo.js
@@ -1,17 +1,39 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
+import { navigate } from '@reach/router';
 
 
 const ProfileInfo = (props) => {
 
-    // How to get current user?
+    const [loggedInUser, setLoggedInUser] = useState('');
 
+    // axios.get('http://localhost:8000/api/user/getLoggedInUser', {
+    //     withCredentials: true
+    // })
+    // .then((res) =>{
+    //     console.log(res.data)
+    //     setLoggedInUser(res.data)
+    // })
+    // .catch((err) =>{
+    //     console.log(err)
+    // })
 
+    
 
     return (
-        <div>
-            <img src="Img placeholder" />
-            <h2>John Doe</h2>
+        <div className='container'>
+            <div className="d-flex align-items-center flex-column">
+                <div className="mt-4">
+                    <i  className="bi bi-person-circle"/>  
+                </div>
+                <div className="mt-4">
+                    {/* <h2>{loggedInUser.firstName} {loggedInUser.lastName}</h2> */}
+                    <h4>Neal Ichinohe</h4>
+                </div>
+                <div className="mt-4">
+                    <button className="btn btn-lg btn-outline-primary"  onClick={() => navigate('/my-blogs')}> My Blogs </button>
+                </div>
+            </div>
         </div>
     )
 }
