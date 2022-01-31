a0c363b5617f6e3655e532cfde8527c77ef1ec9b
nealichinohe <neal.kuni@yahoo.com>
2022-01-30 18:07:38 -0700

    added CSS to profile comp

--- blog/client/src/components/ProfileInfo.js
+++ blog/client/src/components/ProfileInfo.js
@@ -1,38 +1,52 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import { navigate } from '@reach/router';
+import styles from '../styles/BlogList.module.css'
 
 
 const ProfileInfo = (props) => {
     const { user } = props
     const [loggedInUser, setLoggedInUser] = useState('');
+    const [avatar, setAvatar] = useState('');
 
-    axios.get('http://localhost:8000/api/user/getLoggedInUser', {
-        withCredentials: true
-    })
-    .then((res) =>{
-        console.log(res.data)
-        setLoggedInUser(res.data)
-    })
-    .catch((err) =>{
-        console.log(err)
-    })
+    // useEffect(() =>{
+    //     axios.get(`https://ui-avatars.com/api/?name=${loggedInUser.firstName}+${loggedInUser.lastName}`)
+    //         .then((res) =>{
+    //             console.log(loggedInUser)
+    //             console.log(res.json)
+    //             console.log(res.results)
+    //             setAvatar(res.results)
+    //         })
+    // })
 
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/user/getLoggedInUser', {
+            withCredentials: true
+        })
+        .then((res) =>{
+            console.log(res.data)
+            setLoggedInUser(res.data)
+        })
+        .catch((err) =>{
+            console.log(err)
+        })
+    
+    },[])
+    
     
 
     return (
-        <div className='container'>
-            <div className="d-flex align-items-center flex-column">
-                <div className="mt-4">
-                    <i  className="bi bi-person-circle"/>  
+        <div className="container ">
+            <div className={`${styles.blogContainer} rounded mb-2 bg-white p-2`}>
+            <div className="d-flex align-items-center flex-column bg-white">
+                <div className="mt-4 bg-white">
+                    <img className="bg-white" src='../../images/profile.png'/>
                 </div>
                 <div className="mt-4">
-                    <h2>{loggedInUser.firstName} {loggedInUser.lastName}</h2>
-                    <h4>Neal Ichinohe</h4>
+                    <h2 className="bg-white">{loggedInUser.firstName} {loggedInUser.lastName}</h2>
+                    
                 </div>
-                {/* <div className="mt-4">
-                    <button className="btn btn-lg btn-outline-primary"  onClick={() => navigate('/my-blogs')}> My Blogs </button>
-                </div> */}
+            </div>
             </div>
         </div>
     )
